46010ba0a330e439c74f78355ab78d837f3625d4
Stuart MacKay <smackay@flagstonesoftware.com>
2021-12-21 11:23:23 +0000

    Use the changelist template directly rather than overriding it

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -11,3 +11,4 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ["title", "slug", "published"]
     list_filter = [("published", DateRangeFilter)]
     ordering = ["-created"]
+    change_list_template = "admin/daterange/change_list.html"
